6db78850016907c9e64b2d6e63817dd4b6df7be5
dsjong <dsjong2002@yahoo.com>
2021-03-09 19:26:08 +0800

    test

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ bot = commands.Bot(command_prefix='p!')
 #----------Bot-related commands----------
 @bot.event
 async def on_ready():
+	path = Path(__file__).parent
 	print([f.path for f in os.scandir(path) if f.is_dir()])
 	global transformed
 	transformed = False
